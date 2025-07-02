8155deff52f05b51c832633572e0aebb021392d8
yangyile <gogit@github.com/go-xlan/gogit>
2025-07-02 10:42:01 +0700

    让获取键的函数可以返回失败从而直接限流

--- ratekratoslimits/rate_kratos_limits.go
+++ ratekratoslimits/rate_kratos_limits.go
@@ -12,6 +12,7 @@ import (
 	"github.com/orzkratos/authkratos"
 	"github.com/orzkratos/authkratos/authkratosroutes"
 	"github.com/orzkratos/authkratos/internal/utils"
+	"github.com/yyle88/must"
 	"github.com/yyle88/neatjson/neatjsons"
 )
 
@@ -19,7 +20,7 @@ type Config struct {
 	redisCache *redis_rate.Limiter
 	redisLimit *redis_rate.Limit
 	selectPath *authkratosroutes.SelectPath
-	keyFromCtx func(ctx context.Context) string
+	keyFromCtx func(ctx context.Context) (string, bool)
 	debugMode  bool
 }
 
@@ -27,7 +28,7 @@ func NewConfig(
 	redisCache *redis_rate.Limiter,
 	redisLimit *redis_rate.Limit,
 	selectPath *authkratosroutes.SelectPath,
-	keyFromCtx func(ctx context.Context) string,
+	keyFromCtx func(ctx context.Context) (string, bool),
 ) *Config {
 	return &Config{
 		redisCache: redisCache,
@@ -79,21 +80,34 @@ func middlewareFunc(cfg *Config, logger log.Logger) middleware.Middleware {
 
 	return func(handleFunc middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (resp interface{}, err error) {
-			uniqueKey := cfg.keyFromCtx(ctx)
-
-			allowResp, err := cfg.redisCache.Allow(ctx, uniqueKey, *cfg.redisLimit)
-			if err != nil {
-				return nil, errors.ServiceUnavailable("unavailable", "rate-kratos-limits: redis is unavailable").WithCause(err)
+			// 这里就是从上下文中获取唯一键，通常是用户的 PK UK ID 或者 IP 地址等信息
+			uniqueKey, ok := cfg.keyFromCtx(ctx)
+			if !ok {
+				if cfg.debugMode {
+					LOG.Debugf("rate-kratos-limits: reject requests key=unknown missing unique key from context")
+				}
+				return nil, ratelimit.ErrLimitExceed
 			}
 
-			if allowResp.Allowed == 0 {
+			must.Nice(uniqueKey) //避免空键
+
+			// 这块底层包在设计时有 AllowN 的设计，这使得该函数的返回值，还得转换转换 res.Allowed > 0 时才算是通过
+			res, err := cfg.redisCache.Allow(ctx, uniqueKey, *cfg.redisLimit)
+			if err != nil {
 				if cfg.debugMode {
-					LOG.Debugf("rate-kratos-limits: reject requests key=%s allowed=%v remaining=%v", uniqueKey, allowResp.Allowed, allowResp.Remaining)
+					LOG.Debugf("rate-kratos-limits: redis is unavailable key=%s err=%v reject requests", uniqueKey, err)
+				}
+				return nil, errors.ServiceUnavailable("unavailable", "rate-kratos-limits: redis is unavailable").WithCause(err)
+			}
+			// 当然在这种场景里 res.Allowed 的返回值只能是0或1两个值，但在写逻辑时把范围放宽些，避免底层不按预期返回
+			if res.Allowed <= 0 {
+				if cfg.debugMode {
+					LOG.Debugf("rate-kratos-limits: reject requests key=%s allowed=%v remaining=%v", uniqueKey, res.Allowed, res.Remaining)
 				}
 				return nil, ratelimit.ErrLimitExceed
 			}
 			if cfg.debugMode {
-				LOG.Debugf("rate-kratos-limits: accept requests key=%s allowed=%v remaining=%v", uniqueKey, allowResp.Allowed, allowResp.Remaining)
+				LOG.Debugf("rate-kratos-limits: accept requests key=%s allowed=%v remaining=%v", uniqueKey, res.Allowed, res.Remaining)
 			}
 			return handleFunc(ctx, req)
 		}
